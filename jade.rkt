be13363b0ac8d8d4adffe9975363b23332444a5f
Jess Austin <jess.austin@gmail.com>
2015-04-22 06:22:03 -0500

    refactor; element is still eating ]'s

--- jade.rkt
+++ jade.rkt
@@ -7,9 +7,8 @@
   (<or> (try p)
         (return else)))
 
-(define (returnString [wrap identity])
-  (λ (chars)
-    (return (wrap (list->string chars)))))
+(define (returnString [transform identity])
+  (compose return transform list->string))
 
 ; XXX this is quite incomplete
 ; it should include code exec
@@ -38,130 +37,117 @@
            (char #\})
            rightSide))
 
-(define tagInterpolation
+(define tagInterpolation ; XXX prevent element from eating the "]"
   (between (string "#[")
            (char #\])
            (λ (state)  ; element isn't defined yet so wrap in λ
              (element state))))
 
 (define textLine
-  (many1Until (<or> (try escapedInterpolation) ; back out for tagInterpolation
+  (many1Until (<or> (try escapedInterpolation)
                     unescapedInterpolation
                     tagInterpolation
                     text)
-              $eol))
+              (lookAhead $eol)))
 
 (define pipedText
   (>> $spaces
       (>> (string "| ")
-          textLine)))
-
-(define textBlock
-  (parser-compose (indent <- (>>= (lookAhead (many $space))
-                                  (returnString)))
-                  (lines  <- (>>= (many (>> (string indent)
-                                            textLine))
-                                  (λ (lines)
-                                    (return (map car
-                                                 lines)))))
-                  (return (string-join lines))))
+          (>>= textLine
+               (compose return first)))))
 
 ;; node parsers
 (define tagName
-  (>>= (many1 $alphaNum)
-       (returnString string->symbol)))
+  (maybe (>>= (many1 $alphaNum)
+              (returnString string->symbol))
+         'div))
 
 (define attribute
-  (parser-compose (attr <- (>>= (many1 $alphaNum)
-                                (returnString string->symbol)))
-                  (valu <- (maybe (>> (char #\=)
-                                      rightSide)
-                                  (symbol->string attr)))
-                  (return (list attr
-                                valu))))
+  (parser-compose
+    (attr <- (>>= (many1 $alphaNum)
+                  (returnString string->symbol)))
+    (valu <- (maybe (>> (char #\=)
+                        rightSide)
+                    (symbol->string attr)))
+    (return (list attr
+                  valu))))
 
 (define attributes
   (between (char #\()
            (char #\))
            (sepBy1 attribute
                    (many (<or> $space
-                               (char #\,)
-                               $eol)))))
+                               (char #\,))))))
 
+(define andAttributes
+  (between (string "&attributes(")
+           (char #\))
+           rightSide)) ; XXX should iter through the dict
+  
 (define idLiteral
   (>>= (>> (char #\#)
            (many1 $alphaNum))
-       (returnString (λ (id)
-                       (list 'id
-                             id)))))
+       (returnString (curry list 'id))))
 
 (define classLiteral
-  (>>= (>> (char #\.)
-           (many1 $alphaNum))
-       (returnString (λ (class)
-                       (list 'class
-                             class)))))
+  (>> (try (lookAhead (>> (char #\.)
+                          $alphaNum)))
+      (>>= (>> (char #\.)
+               (many1 $alphaNum))
+           (returnString (curry list 'class)))))
 
-(define literals
-  (>>= (parser-seq (many classLiteral)
-                   (>>= (maybe idLiteral)
-                        (λ (id)
-                          (return (if (null? id)
-                                      id
-                                      (list id)))))
-                   (many classLiteral))
-       (λ (literals)
-         (return (apply append
-                        literals)))))
-
-(define divLiteral
-  (>>= (parser-seq literals
-                   attributes)
-       (λ (attributes)
-         (return (list 'div
-                       (apply append
-                              attributes))))))
+(define (leaveNull item transform)
+  (if (null? item)
+      item
+      (transform item)))
 
 (define tag
-  (>>= (<or> divLiteral
-             (parser-compose (t  <- tagName)
-                             (a1 <- (maybe literals))
-                             (a2 <- (maybe attributes))
-                             (return (list t
-                                           (append a1
-                                                   a2)))))
-       ; more complicated than we'd like, since list-no-order can't use
-       ; multiple ...
-       (match-lambda [(list tag attrs)
-                      (let*-values ([(class rest)
-                                     (partition (λ (item)
-                                                  (equal? (car item)
-                                                          'class))
-                                                attrs)]
-                                    [(classes) (map second class)])
-                        (return (list tag
-                                      (append rest
-                                              (if (null? classes)
-                                                  null
-                                                  (list (list 'class
-                                                              (string-join classes))))))))])))
+  (parser-compose
+    (tn <- tagName)
+    (c1 <- (many classLiteral))
+    (id <- (maybe idLiteral))
+    (c2 <- (many classLiteral))
+    (a1 <- (maybe attributes))
+    (a2 <- (maybe andAttributes))
+    (return
+      (let*-values ([(attrs) (append a1 a2)]
+                    [(clattrs rest) (partition (compose (curry equal? 'class)
+                                                        first)
+                                               attrs)]
+                    [(classes) (map second (append c1 c2 clattrs))])
+        (list tn
+              (append (leaveNull id
+                                 list)
+                      (leaveNull classes
+                                 (compose list
+                                          (curry list 'class)
+                                          string-join))
+                      rest))))))
+
+(define (indentAtLeast spaces p)
+  (>> (try (lookAhead (>> $eol
+                          (>> (string (list->string spaces))
+                              (many1 $space)))))
+      (>> $eol
+          p)))
 
 (define element
-  (parser-compose (indent <- (>>= (many $space)
-                                  (returnString)))
-                  (tagAtt <- tag)
-                  (chldrn <- (maybe (<or> (parser-seq (maybe (>> $space
-                                                                 textLine))
-                                                      (~ $eol)
-                                                      (~ (lookAhead (string indent)))
-                                                      (many (<or> pipedText
-                                                                  element)))
-                                          (parser-seq (~ (char #\.))
-                                                      (~ $eol)
-                                                      (~ (lookAhead (string indent)))
-                                                      textBlock))))
-                  (return (append tagAtt
-                                  chldrn))))
+  (parser-compose
+    (indent <- (many (char #\space)))
+    (tagAtt <- tag)
+    (chldrn <- (<or> (>> (char #\.)
+                         (many (indentAtLeast indent
+                                              (>> $spaces
+                                                  textLine))))
+                     (>>= (parser-seq
+                            (maybe (>> (char #\space)
+                                       textLine))
+                            (many (indentAtLeast indent
+                                                 (<or> (try pipedText)
+                                                       element))))
+                          (compose return (curry apply append)))))
+    (return (append tagAtt
+                    chldrn))))
 
 ;(parse textLine "this is text, #[bar], and more text.\n")
 ;(parse tagInterpolation "#[baz]")
@@ -183,6 +169,8 @@
 ;(parse element "p(baz=\"bak\" foo=\"bar\") abc def ghi\n")
 ;(parse element ".bax#goo.bar abc def ghi\n")
 ;(parse element "p(baz=\"bak\") abc #[foo] def #[bar] ghi\n")
-;(parse element ".po#goo.kob(baz=\"bak\", nab=\"ban\") abc #[foo] def #[bar.gee] ghi\n")
-(parse element "p.\n  This is text.\n  This is more.\n")
-(parse textBlock "  This is text.\n")
+;(parse textLine "abc #[foo] def #[bar.gee] ghi\n")
+(parse element ".po#goo.kob(baz=\"bak\",\n nab=\"ban\" class=\"extra\") abc #[foo] def #[bar.gee] ghi\n")
+(parse element "p This is text.\n  | This is more.\n  span#here More\n  | text.\n")
+(parse element "p.\n  This is more.\n  Now #[span#here more] text.\n")
+;(parse textBlock "  This is text.\n")
